61e102337b02cffa80aa1a73d31289522caf6c37
Paulo Belo <pjbelo@gmail.com>
2020-07-01 00:17:51 +0100

    config new database and use env password

--- config/connectMySQL.js
+++ config/connectMySQL.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 module.exports = {
-	con: mysql.createConnection({
-		host     : 'webitcloud.net',
-		user     : 'webitclo_webbook',
-		password : 'webbookPW#2018',
-		database : 'webitclo_webbook'
-	})
+  con: mysql.createConnection({
+    host: "zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    user: "wa4katvxw5sg00gl",
+    password: process.env.DBPASS,
+    database: "iwp1vd0b9x4ja6z5",
+  }),
 };
--- models/index.js
+++ models/index.js
@@ -1,22 +1,26 @@
 "use strict";
-let fs        = require("fs");
-let path      = require("path");
+let fs = require("fs");
+let path = require("path");
 let Sequelize = require("sequelize");
-let env       = "MySQL";
-let config    = require(path.join(__dirname, '../', 'config', 'config.json'))[env];
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
-let db        = {};
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
+let env = "MySQL";
+let config = require(path.join(__dirname, "../", "config", "config.json"))[env];
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  process.env.DBPASS,
+  config
+);
+let db = {};
+fs.readdirSync(__dirname)
+  .filter(function (file) {
+    return file.indexOf(".") !== 0 && file !== "index.js";
   })
-  .forEach(function(file) {
+  .forEach(function (file) {
     let model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(function (modelName) {
   if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
